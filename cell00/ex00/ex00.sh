ce3374d373239d624aac02363f74799bdadcdf00
Dearnarakorn <madear5659@gmail.com>
2025-02-03 22:22:58 +0700

    ex00

---
+++ cell00/ex00/ex00.sh
@@ -0,0 +1,10 @@
+# Create discovery_piscine at Home Root
+mkdir /discovery_piscine
+cd /discovery_piscine
+
+# Create cell00 and Move Into It
+mkdir cell00
+cd cell00
+
+# Create ex00 Folder
+mkdir ex00
