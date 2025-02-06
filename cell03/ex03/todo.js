c5852650d14f9e429acc2ee76dfd251c1f617ce3
Dearnarakorn <madear5659@gmail.com>
2025-02-07 00:43:48 +0700

    3.3

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,47 +1,13 @@
-const myNodelist = document.getElementById("ft_list");
-
-// Create a new To-Do
-function createToDo(txt) {
-  const node = document.createElement("div");
-  node.textContent = txt;
-
-  node.addEventListener("click", function () {
-    if (confirm(`Are you sure you want to delete: "${txt}"?`)) {
-      myNodelist.removeChild(node);
-      saveTasks();
+function addTask() {
+    let task = prompt("Enter a task:");
+    if (task) {
+        let li = document.createElement("li");
+        li.textContent = task;
+        li.onclick = function() {
+            if (confirm("Remove this task?")) {
+                this.remove();
+            }
+        };
+        document.getElementById("taskList").appendChild(li);
     }
-  });
-
-  return node;
 }
-
-// Prompt for new To-Do
-function popup() {
-  const task = prompt("Enter a new task...");
-  if (task && task.trim() !== "") {
-    const newTask = createToDo(task.trim());
-    myNodelist.prepend(newTask);
-    saveTasks();
-  }
-}
-
-// Save tasks to cookies
-function saveTasks() {
-  const tasks = [...myNodelist.getElementsByTagName("div")].map(x => x.textContent);
-  document.cookie = "Tasks=" + encodeURIComponent(JSON.stringify(tasks)) + ";path=/";
-}
-
-// Load tasks from cookies
-(function loadTasks() {
-  const cookies = document.cookie.split(";").reduce((acc, pair) => {
-    const [key, ...vals] = pair.trim().split("=");
-    acc[key] = vals.join("=");
-    return acc;
-  }, {});
-
-  if (cookies.Tasks) {
-    JSON.parse(decodeURIComponent(cookies.Tasks)).forEach(task => {
-      myNodelist.append(createToDo(task));
-    });
-  }
-})();
